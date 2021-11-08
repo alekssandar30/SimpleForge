0c154abcc7d8cc607f315aa8c905c2ad5e7a2f15
unknown <sale.novakovic403@gmail.com>
2021-11-08 22:23:34 +0100

    implemented basic api for uploading and translating 3d models

---
+++ TestForgeApp/Controllers/OAuthController.cs
@@ -0,0 +1,66 @@
+﻿using Autodesk.Forge;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace forgeSample.Controllers
+{
+    // simple auth implemented here...
+
+    [ApiController]
+    public class OAuthController : ControllerBase
+    {
+
+        private static dynamic InternalToken { get; set; }
+        private static dynamic PublicToken { get; set; }
+
+        [HttpGet]
+        [Route("api/forge/oauth/token")]
+        public async Task<dynamic> GetPublicAsync()
+        {
+            if (PublicToken == null || PublicToken.ExpiresAt < DateTime.UtcNow)
+            {
+                PublicToken = await Get2LeggedTokenAsync(new Scope[] { Scope.ViewablesRead });
+                PublicToken.ExpiresAt = DateTime.UtcNow.AddSeconds(PublicToken.expires_in);
+            }
+            return PublicToken;
+        }
+
+        /// <summary>
+        /// Get access token with internal (write) scope
+        /// </summary>
+        public static async Task<dynamic> GetInternalAsync()
+        {
+            if (InternalToken == null || InternalToken.ExpiresAt < DateTime.UtcNow)
+            {
+                InternalToken = await Get2LeggedTokenAsync(new Scope[] { Scope.BucketCreate, Scope.BucketRead, Scope.BucketDelete, Scope.DataRead, Scope.DataWrite, Scope.DataCreate, Scope.CodeAll });
+                InternalToken.ExpiresAt = DateTime.UtcNow.AddSeconds(InternalToken.expires_in);
+            }
+
+            return InternalToken;
+        }
+
+        /// <summary>
+        /// Get the access token from Autodesk
+        /// </summary>
+        private static async Task<dynamic> Get2LeggedTokenAsync(Scope[] scopes)
+        {
+            TwoLeggedApi oauth = new TwoLeggedApi();
+            string grantType = "client_credentials";
+            dynamic bearer = await oauth.AuthenticateAsync(
+              GetAppSetting("FORGE_CLIENT_ID"),
+              GetAppSetting("FORGE_CLIENT_SECRET"),
+              grantType,
+              scopes);
+            return bearer;
+        }
+
+        /// <summary>
+        /// Reads appsettings from web.config
+        /// </summary>
+        public static string GetAppSetting(string settingKey)
+        {
+            return Environment.GetEnvironmentVariable(settingKey).Trim();
+        }
+    }
+}
---
+++ TestForgeApp/Startup.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestForgeApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddNewtonsoftJson();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
