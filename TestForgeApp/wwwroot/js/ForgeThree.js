e10d5b32dc0b1f7ecc3a25b619c76e0916a106ae
unknown <sale.novakovic403@gmail.com>
2022-01-12 10:46:18 +0100

    bim360 integration

--- TestForgeApp/wwwroot/js/ForgeThree.js
+++ TestForgeApp/wwwroot/js/ForgeThree.js
@@ -1,29 +1,33 @@
 ﻿$(document).ready(function () {
 
     // first, check if current visitor is signed in
-    //jQuery.ajax({
-    //    url: '/api/forge/oauth/token',
-    //    success: function (res) {
-    //        $('#signOut').show();
-    //        $('#refreshHubs').show();
+    jQuery.ajax({
+        url: '/api/forge/oauth/token',
+        success: function (res) {
+            // yes, it is signed in...
+            $('#signOut').show();
+            $('#refreshHubs').show();
 
-    //        // prepare sign out
-    //        $('#signOut').click(function () {
-    //            $('#hiddenFrame').on('load', function (event) {
-    //                location.href = '/api/forge/oauth/signout';
-    //            });
-    //            $('#hiddenFrame').attr('src', 'https://accounts.autodesk.com/Authentication/LogOut');
-    //        })
+            // prepare sign out
+            $('#signOut').click(function () {
+                $('#hiddenFrame').on('load', function (event) {
+                    location.href = '/api/forge/oauth/signout';
+                });
+                $('#hiddenFrame').attr('src', 'https://accounts.autodesk.com/Authentication/LogOut');
+                // learn more about this signout iframe at
+                // https://forge.autodesk.com/blog/log-out-forge
+            })
 
-    //        // and refresh button
-    //        $('#refreshHubs').click(function () {
-    //            $('#userHubs').jstree(true).refresh();
-    //        });
+            // and refresh button
+            $('#refreshHubs').click(function () {
+                $('#userHubs').jstree(true).refresh();
+            });
 
-    //        prepareUserHubsTree();
-    //        showUser();
-    //    }
-    //});
+            // finally:
+            prepareUserHubsTree();
+            showUser();
+        }
+    });
 
     $('#autodeskSigninButton').click(function () {
         jQuery.ajax({
@@ -34,22 +38,31 @@
         });
     })
 
-    prepareAppBucketTree();
-    $('#refreshBuckets').click(function () {
-        $('#appBuckets').jstree(true).refresh();
+    $.getJSON("/api/forge/clientid", function (res) {
+        $("#ClientID").val(res.id);
+        $("#provisionAccountSave").click(function () {
+            $('#provisionAccountModal').modal('toggle');
+            $('#userHubs').jstree(true).refresh();
+        });
     });
+});
 
-    $('#createNewBucket').click(function () {
-        createNewBucket();
-    });
+    //prepareAppBucketTree();
+    //$('#refreshBuckets').click(function () {
+    //    $('#appBuckets').jstree(true).refresh();
+    //});
 
-    $('#createBucketModal').on('shown.bs.modal', function () {
-        $("#newBucketKey").focus();
-    })
+    //$('#createNewBucket').click(function () {
+    //    createNewBucket();
+    //});
 
-    $('#snapshotBtn').click(function () {
-        snapshot();
-    })
+    //$('#createBucketModal').on('shown.bs.modal', function () {
+    //    $("#newBucketKey").focus();
+    //})
+
+    //$('#snapshotBtn').click(function () {
+    //    snapshot();
+    //})
 
 
     // ***************************************** UPLOAD TO BUCKET **************************************
@@ -104,7 +117,7 @@
         }
 
     });
-});
+
 
 // ******************************** CREATE A BUCKET ****************************************
 
@@ -341,6 +354,7 @@ function deleteObject(node) {
 
 
 function prepareUserHubsTree() {
+    var haveBIM360Hub = false;
     $('#userHubs').jstree({
         'core': {
             'themes': { "icons": true },
@@ -352,43 +366,78 @@ function prepareUserHubsTree() {
                 'data': function (node) {
                     $('#userHubs').jstree(true).toggle_node(node);
                     return { "id": node.id };
+                },
+                "success": function (nodes) {
+                    nodes.forEach(function (n) {
+                        if (n.type === 'bim360Hubs' && n.id.indexOf('b.') > 0)
+                            haveBIM360Hub = true;
+                    });
+
+                    if (!haveBIM360Hub) {
+                        $("#provisionAccountModal").modal();
+                        haveBIM360Hub = true;
+                    }
                 }
             }
         },
         'types': {
-            'default': { 'icon': 'glyphicon glyphicon-question-sign' },
-            '#': { 'icon': 'glyphicon glyphicon-user' },
-            'hubs': { 'icon': 'https://github.com/Autodesk-Forge/bim360appstore-data.management-nodejs-transfer.storage/raw/master/www/img/a360hub.png' },
-            'personalHub': { 'icon': 'https://github.com/Autodesk-Forge/bim360appstore-data.management-nodejs-transfer.storage/raw/master/www/img/a360hub.png' },
-            'bim360Hubs': { 'icon': 'https://github.com/Autodesk-Forge/bim360appstore-data.management-nodejs-transfer.storage/raw/master/www/img/bim360hub.png' },
-            'bim360projects': { 'icon': 'https://github.com/Autodesk-Forge/bim360appstore-data.management-nodejs-transfer.storage/raw/master/www/img/bim360project.png' },
-            'a360projects': { 'icon': 'https://github.com/Autodesk-Forge/bim360appstore-data.management-nodejs-transfer.storage/raw/master/www/img/a360project.png' },
-            'folders': { 'icon': 'glyphicon glyphicon-folder-open' },
-            'items': { 'icon': 'glyphicon glyphicon-file' },
-            'bim360documents': { 'icon': 'glyphicon glyphicon-file' },
-            'versions': { 'icon': 'glyphicon glyphicon-time' },
-            'unsupported': { 'icon': 'glyphicon glyphicon-ban-circle' }
+            'default': {
+                'icon': 'glyphicon glyphicon-question-sign'
+            },
+            '#': {
+                'icon': 'glyphicon glyphicon-user'
+            },
+            'hubs': {
+                'icon': 'https://github.com/Autodesk-Forge/learn.forge.viewhubmodels/raw/master/img/a360hub.png'
+            },
+            'personalHub': {
+                'icon': 'https://github.com/Autodesk-Forge/learn.forge.viewhubmodels/raw/master/img/a360hub.png'
+            },
+            'bim360Hubs': {
+                'icon': 'https://github.com/Autodesk-Forge/learn.forge.viewhubmodels/raw/master/img/bim360hub.png'
+            },
+            'bim360projects': {
+                'icon': 'https://github.com/Autodesk-Forge/learn.forge.viewhubmodels/raw/master/img/bim360project.png'
+            },
+            'a360projects': {
+                'icon': 'https://github.com/Autodesk-Forge/learn.forge.viewhubmodels/raw/master/img/a360project.png'
+            },
+            'items': {
+                'icon': 'glyphicon glyphicon-file'
+            },
+            'bim360documents': {
+                'icon': 'glyphicon glyphicon-file'
+            },
+            'folders': {
+                'icon': 'glyphicon glyphicon-folder-open'
+            },
+            'versions': {
+                'icon': 'glyphicon glyphicon-time'
+            },
+            'unsupported': {
+                'icon': 'glyphicon glyphicon-ban-circle'
+            }
         },
         "sort": function (a, b) {
             var a1 = this.get_node(a);
             var b1 = this.get_node(b);
             var parent = this.get_node(a1.parent);
-            if (parent.type === 'items') { // sort by version number
+            if (parent.type === 'items') {
                 var id1 = Number.parseInt(a1.text.substring(a1.text.indexOf('v') + 1, a1.text.indexOf(':')))
                 var id2 = Number.parseInt(b1.text.substring(b1.text.indexOf('v') + 1, b1.text.indexOf(':')));
                 return id1 > id2 ? 1 : -1;
             }
-            else if (a1.type !== b1.type) return a1.icon < b1.icon ? 1 : -1; // types are different inside folder, so sort by icon (files/folders)
-            else return a1.text > b1.text ? 1 : -1; // basic name/text sort
+            else return a1.text > b1.text ? 1 : -1;
         },
         "plugins": ["types", "state", "sort"],
         "state": { "key": "autodeskHubs" }// key restore tree state
     }).bind("activate_node.jstree", function (evt, data) {
         if (data != null && data.node != null && (data.node.type == 'versions' || data.node.type == 'bim360documents')) {
-            // in case the node.id contains a | then split into URN & viewableId
+            var urn;
+            var viewableId
             if (data.node.id.indexOf('|') > -1) {
-                var urn = data.node.id.split('|')[1];
-                var viewableId = data.node.id.split('|')[2];
+                urn = data.node.id.split('|')[1];
+                viewableId = data.node.id.split('|')[2];
                 launchViewer(urn, viewableId);
             }
             else {
@@ -397,6 +446,7 @@ function prepareUserHubsTree() {
         }
     });
 }
+
 
 function showUser() {
     jQuery.ajax({
